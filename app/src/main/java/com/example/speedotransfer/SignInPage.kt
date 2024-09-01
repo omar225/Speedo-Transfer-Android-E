aeecc8e648d19347368c8ca18fda3b82851e0f70
Omar Mohamed <omarmohmd2003@gmail.com>
2024-09-01 21:43:59 +0300

    added the sign in and timeout pages. changed the icons from .png to .svg fine-tuned the input fields. put the input fields in a separate file for reuse

--- app/src/main/java/com/example/speedotransfer/SignInPage.kt
+++ app/src/main/java/com/example/speedotransfer/SignInPage.kt
@@ -11,19 +11,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,20 +29,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.speedotransfer.ui.theme.AppTypography
-import com.example.speedotransfer.ui.theme.D300
 import com.example.speedotransfer.ui.theme.G0
-import com.example.speedotransfer.ui.theme.G10
 import com.example.speedotransfer.ui.theme.G100
 import com.example.speedotransfer.ui.theme.G40
-import com.example.speedotransfer.ui.theme.G70
-import com.example.speedotransfer.ui.theme.G700
 import com.example.speedotransfer.ui.theme.Login
 import com.example.speedotransfer.ui.theme.P300
 
@@ -239,5 +227,6 @@ fun TimeOut(modifier: Modifier = Modifier) {
 @Composable
 fun SignUpPreview() {
     SignIn()
+    TimeOut()
 }
 
--- app/src/main/java/com/example/speedotransfer/SignUpScreen.kt
+++ app/src/main/java/com/example/speedotransfer/SignUpScreen.kt
@@ -1,6 +1,5 @@
 package com.example.speedotransfer
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,30 +9,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
@@ -255,4 +242,6 @@ fun SecondSignUp(modifier: Modifier = Modifier) {
 @Composable
 fun LoginScreenPreview() {
     SecondSignUp()
+    Splash()
+    SignUp()
 }
